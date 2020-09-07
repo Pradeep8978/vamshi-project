7047f7f3bb4ee0e26c239527d1e3599da5c88418
Pradeep Reddy <easypradeep@outlook.com>
2020-09-08 02:06:17 +0530

    Css changes

--- src/containers/AboutUs/AboutUs.js
+++ src/containers/AboutUs/AboutUs.js
@@ -1,46 +1,59 @@
-import React, { Component } from 'react'
-import './AboutUs.css'
-import Logo from './../../images/walogo.png'
-// import NavBar from "./../../Layouts/NavBar/NavBar"
+import React, { Component } from "react";
+import "./AboutUs.scss";
+import Logo from "./../../images/walogo.png";
+import NavBar from "./../../Layouts/NavBar/NavBar";
+import Rotation from "./../../components/Rotation/Rotation";
 
 class AboutUs extends Component {
-    render() {
-        return (
-            <>
-
-            <main>
-                <section className="wrapper">
-                    <div className="img-blog">
-                        <img src={Logo} width="100%" height="450px" />
-                    </div>
-                    <div className="text-blog">
-                        <div>
-                            <p><b>In this Chose Content exist in between  <br></br> brands and customers today</b></p>
-                            <h2>WISEAPE is here to and some order.</h2>
-                        </div>
-                    </div>
-                </section>
-                <section className="middleBlog">
-                    <div>
-                        <h2>WISEAPE is here to and some order.<br /> WISEAPE is here to
-                        WISEAPE is here to and some order.<br /> WISEAPE is here
-                        </h2>
-                        <p>Warangal (About this soundpronunciation
-                        (help·info)) is a city in the Indian state of Telangana. It serves as the district headquarters of both Warangal Urban District and Warangal Rural District.[5] With a population of 830,281 per 2011 Census of India,[3][4] and spreading over an 406 km2 (157 sq mi),[1]
-                             Warangal is the second most populous city in the state after the capital Hyderabad.</p>
-                    </div>
-                </section>
-                <footer>
-                    footer
-                </footer>
-            </main>
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <main className="about-us-wrapper">
+          <section className="wrapper">
+            <NavBar />
+            <div className="img-blog">
+              <img src={Logo} width="100%" height="450px" />
+            </div>
+            <div className="text-blog">
+              <div className="text-content">
+                <p>
+                  <b>
+                    In this Chose Content exist in between <br></br> brands and customers today
+                  </b>
+                </p>
+                <h2>WISEAPE is here to and some order.</h2>
+              </div>
+            </div>
+            <Rotation text=". &nbsp;&nbsp;WE ARE WISEAPE	&nbsp; .	&nbsp; TAG LINE COMES " />
+          </section>
+          <section className="middleBlog">
+            <div className="container">
+              <h2>
+                WISEAPE is here to to help brands
+                <br /> discover their purpose as a fellow human and
+                <br /> make things of importance.
+              </h2>
+              <p>
+                <div>
+                  Pioneering a new kind of relationship between the brands and the people that
+                  follow them.
+                </div>
+                <div>
+                  We stay true to the brands and carve out a place for the brand in the culture that
+                  our audience lives in.
+                </div>
+                <div>
+                  Your audience wants ownership of your brand and we are here to help you amke that
+                  transition and reap the commercial success that comes along with it.
+                </div>
+              </p>
+            </div>
+          </section>
+          <footer>footer</footer>
+        </main>
+      </>
+    );
+  }
 }
 
 export default AboutUs;
-
-
-
-
