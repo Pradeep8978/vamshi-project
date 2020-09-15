39162d982161966e73fa35392309fe79efe11554
Ganeshreddykatla <ganeshreddykatla@gmail.com>
2020-09-15 23:18:46 +0530

    my works ganesh

--- src/containers/Features/Work.js
+++ src/containers/Features/Work.js
@@ -1,32 +1,63 @@
 import React, { useState } from "react";
 import { Modal, Button } from "react-bootstrap";
 import ReactPlayer from "react-player";
+
 import "./Work.scss";
 import Carousal from "./../../Layouts/Carousal/Carousal";
 
 const videoConfig = [
   {
     name: "Axis - Do Your Home Work",
+    image: require("../../images/workimages/axis.JPG"),
+    step: "STEP:1 MINIMUM WORK : DIGITAL EXPERIENCE",
+    heading1: '#LoveImporttwo-',
+    heading2: '1 Love Both, 200Dates',
+    description: "We help brands become a real and meaningful contributor to our lives.",
     video: "https://www.youtube.com/watch?v=g0Y3opLVAc0",
   },
   {
     name: "British Council - IELTS Prep App",
+    image: require("../../images/workimages/darkfantasy.JPG"),
+    step: "STEP:1 MINIMUM WORK : DIGITAL EXPERIENCE",
+    heading1: '#LoveImporttwo-',
+    heading2: '1 Love Both, 200Dates',
+    description: "We help brands become a real and meaningful contributor to our lives.",
     video: "https://www.youtube.com/watch?v=1HSZPnv5_W8&t=25s",
   },
   {
     name: "Dark Fantasy - Lot Can Happen  Inside",
+    image: require("../../images/workimages/dhl.JPG"),
+    step: "STEP:1 MINIMUM WORK : DIGITAL EXPERIENCE",
+    heading1: '#LoveImporttwo-',
+    heading2: '1 Love Both, 200Dates',
+    description: "We help brands become a real and meaningful contributor to our lives.",
     video: "https://www.youtube.com/watch?v=nKx1Pu6mzKQ&t=15s",
   },
   {
     name: "DHL - Chant for Teams",
+    image: require("../../images/workimages/engage1.JPG"),
+    step: "STEP:1 MINIMUM WORK : DIGITAL EXPERIENCE",
+    heading1: '#LoveImporttwo-',
+    heading2: '1 Love Both, 200Dates',
+    description: "We help brands become a real and meaningful contributor to our lives.",
     video: "https://www.youtube.com/watch?v=q6GH6IgcUDA&t=13s",
   },
   {
     name: "Engage - Blind Dating Booth",
+    image: require("../../images/workimages/toffel.JPG"),
+    step: "STEP:1 MINIMUM WORK : DIGITAL EXPERIENCE",
+    heading1: '#LoveImporttwo-',
+    heading2: '1 Love Both, 200Dates',
+    description: "We help brands become a real and meaningful contributor to our lives.",
     video: "https://www.youtube.com/watch?v=bQv2MgFniig&t=15s",
   },
   {
     name: "Engage - Pocket Full Of Stories 2.0",
+    image: require("../../images/workimages/engage2.JPG"),
+    step: "STEP:1 MINIMUM WORK : DIGITAL EXPERIENCE",
+    heading1: '#LoveImporttwo-',
+    heading2: '1 Love Both, 200Dates',
+    description: "We help brands become a real and meaningful contributor to our lives.",
     video: "https://www.youtube.com/watch?v=rBmcCRrf5ZU",
   },
 ];
@@ -62,7 +93,7 @@ const WorkComponent = () => {
           <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header> */}
         <Modal.Body>
-          <ReactPlayer url={selectedItem.video} />
+          <ReactPlayer playing="false" url={selectedItem.video} />
           {/* <video controls className="video-player" autoPlay>
             <source src={selectedItem.video} type="video/mp4" />
           </video> */}
@@ -89,8 +120,8 @@ const WorkComponent = () => {
         .map((item) => {
           return (
             <div className="secondBlog">
-              <div className="img-wrapper">
-                <img src="https://images.unsplash.com/photo-1513031300226-c8fb12de9ade?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9" />
+              <div className="img-wrapper" style={{ backgroundImage:`url(${item.image})` }}>
+                
                 <div className="image-layer"></div>
                 <div className="nav-buttons">
                   <i class="fa fa-chevron-left" aria-hidden="true" onClick={onDecrement}></i>
@@ -99,13 +130,13 @@ const WorkComponent = () => {
               </div>
               <div className="textBlog">
                 <div>
-                  <p className="step-content">STEP:1 MINIMUM WORK : DIGITAL EXPERIENCE</p>
+          <p className="step-content"> {item.step}</p>
                   <h2>
-                    #LoveImporttwo-
-                    <br />1 Love Both, 200Dates
+                   {item.heading1}
+                    <br />{item.heading2}
                   </h2>
                   <p className="step-content-two">
-                    We help brands become a real and meaningful contributor to our lives.
+                    {item.description}
                   </p>
                   <button onClick={() => onClickButton(item)}>SEE HOW</button>
                 </div>
