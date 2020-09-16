ff621eee36bdea04838afe34fb5813edf59359ec
narren <narren@gmail.com>
2020-09-16 23:11:39 +0530

    changed the css

--- src/Layouts/Years/Years.js
+++ src/Layouts/Years/Years.js
@@ -5,7 +5,7 @@ class Years extends Component {
     return (
       <div>
         <div id="app">
-          <span style={{ fontSize: "60px", fontWeight:"500" }}>3</span>
+          <span style={{ fontSize: "60px", fontWeight:"600",fontFamily:"sans-serif" }}>3</span>
           <span className="years">YRS</span>
         </div>
       </div>
